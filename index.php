57c9ac4ccd9b53aea6e6fb0a374005682109a2ce
Giovanni <dibellogio@gmail.com>
2025-04-24 16:23:05 +0200

    add form filter parking

--- index.php
+++ index.php
@@ -9,52 +9,60 @@
     <title>PHP Hotel</title>
 </head>
 
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
 <body>
-    <h1 class="text-center">Lista Hotel</h1>
+    <h1 class="text-center m-4">Lista Hotel</h1>
 
-    <?php
+    <form class="text-center mb-3" action="" method=GET>
+        <label for="">Parcheggio</label>
+        <input type="checkbox" name="parking" id="true" <?php echo isset($_GET['parking']) ? 'checked' : '' ?>>
+        <button class="btn btn-outline-primary btn-sm" type="submit">Cerca</button>
+    </form>
 
-    $hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
-    ?>
     <div class="container">
         <table class="table table-striped table-bordered">
             <thead>
@@ -69,7 +77,13 @@
             <tbody>
                 <?php
 
-                foreach ($hotels as $hotel) {
+                $filtered = $hotels;
+
+                if (isset($_GET['parking'])) {
+                    $filtered = array_filter($hotels, fn($hotel) => $hotel['parking'] === true);
+                }
+
+                foreach ($filtered as $hotel) {
 
                     echo "<tr>";
                     foreach ($hotel as $key => $value) {
